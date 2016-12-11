8e29ba44f44ffc97b30fd80e1faaecc9d1ae13ff
kvark161 <kvark161@gmail.com>
2016-12-11 16:18:16 +0300

    add list and get by id requests

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,38 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
+import Data.Monoid ((<>))
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics
 import Web.Scotty
 
-main = scotty 3000 $ do
-  get "/" $ do
-    html "Hello World!"
+data Todo = Todo { todoId :: Int, desc :: String } deriving (Show, Generic)
+instance ToJSON Todo
+instance FromJSON Todo
+
+todo1 :: Todo
+todo1 = Todo { todoId = 1, desc = "description 1" }
+
+todo2 :: Todo
+todo2 = Todo { todoId = 2, desc = "description 2" }
+
+todos :: [Todo]
+todos = [todo1, todo2]
+
+matchesId :: Int -> Todo -> Bool
+matchesId inId todo = todoId todo == inId
+
+main = do
+  scotty 3000 $ do
+    get "/hello/:name" $ do
+        name <- param "name"
+        text ("hello " <> name <> "!")
+
+    get "/list" $ do
+      json todos
+
+    get "/todo/:id" $ do
+      inId <- param "id"
+      json (filter (matchesId inId) todos)
