4dad009eec8b05d440a8a6454272329d1d53ffb7
kvark161 <kvark161@gmail.com>
2016-12-23 18:52:57 +0300

    move configuration to fil

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Utils (
         getCurrentLocalTime,
@@ -9,6 +10,19 @@ import Data.Time.LocalTime
 import Data.Time.Clock
 import Data.Pool
 import Database.MySQL.Base
+import Data.Aeson (FromJSON, ToJSON, decode)
+import GHC.Generics
+import qualified Data.ByteString.Lazy.Char8 as Char8
+import qualified Data.ByteString.Char8 as C
+
+data AppConfiguration = AppConfiguration {  userName :: String,
+                                            password :: String,
+                                            databaseName :: String,
+                                            stripeSize :: Int,
+                                            connectionLife :: NominalDiffTime,
+                                            poolSize :: Int} deriving (Generic)
+
+instance FromJSON AppConfiguration
 
 getCurrentLocalTime :: IO LocalTime
 getCurrentLocalTime = do
@@ -17,10 +31,17 @@ getCurrentLocalTime = do
     return $ utcToLocalTime currentTimeZone ct
 
 connectAndRun fun = do
-  pool <- createPool createNewConnection close 10 60 10
-  fun pool
+    config <- readConfiguration
+    pool <- createPool (createNewConnection config) close (stripeSize config) (connectionLife config) (poolSize config)
+    fun pool
 
-createNewConnection = connect defaultConnectInfoMB4 {
-                            ciUser = "root",
-                        ciPassword = "mysql",
-                        ciDatabase = "todolist"}
+createNewConnection config = connect defaultConnectInfoMB4 {
+                            ciUser = C.pack (userName config),
+                        ciPassword = C.pack (password config),
+                        ciDatabase = C.pack (databaseName config)}
+
+readConfiguration = do
+            d <- readFile "config.json"
+            case decode (Char8.pack d) :: Maybe AppConfiguration of
+                Just config -> return config
+                Nothing -> error "configuration is incorrect"
